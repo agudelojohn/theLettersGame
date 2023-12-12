1525401ea8285f6639a94b006935447da53a73b7
John Agudelo <jhbr08@hotmail.com>
2023-12-12 15:28:15 -0500

    Winner view improved

--- components/TilesList/WinnerView.tsx
+++ components/TilesList/WinnerView.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
+import "./winnerView.css";
+
 export const WinnerView = () => {
+  const [size, setSize] = useState("smallText");
+  useEffect(() => {
+    setTimeout(() => {
+      setSize("bigText");
+      setTimeout(() => {
+        setSize("smallText");
+      }, 1000);
+    }, 10);
+  }, []);
   return (
     <>
-      <h1>Congrats! You are the winner!</h1>
+      <h1 className={`text ${size}`}>Congrats! You are the winner!</h1>
     </>
   );
 };
